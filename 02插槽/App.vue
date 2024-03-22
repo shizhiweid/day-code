a16036f6e43fdfcffa319705460885eb6389496a
shi <1613285977@qq.com>
2024-03-22 10:10:53 +0800

    插槽

---
+++ 02插槽/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <Home></Home>
+  </div>
+</template>
+
+<script  setup>
+import Home from "./views/home.vue";
+</script>
---
+++ 02插槽/views/home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>插槽使用</h3>
+    <Slot>
+      <div>1.默认插槽</div>
+      <template #name>
+        <div>2.具名插槽</div>
+      </template>
+
+      <template #slot3="{ text, count }">
+        <div>3.作用域插槽-{{ text }}-{{ count }}</div>
+      </template>
+      
+      <template #[name]> 
+        <div>
+          4.动态插槽
+        </div>
+      </template>
+    </Slot>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive, onMounted } from "vue";
+import Slot from "../components/slot.vue";
+const name="bottom"
+</script>
+<style scoped lang='less'></style>
