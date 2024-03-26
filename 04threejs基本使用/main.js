2a75c8f3ca7bf107d29c693a6b0af6aab0c3c5e1
shi <1613285977@qq.com>
2024-03-26 17:03:17 +0800

    axios封装

---
+++ 04threejs基本使用/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import './style.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import App from './App.vue'
+
+createApp(App).use(ElementPlus, {
+    locale: zhCn
+}).use(ElementPlus).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 
+  server: {
+    // 配置代理
+    proxy: {
+      '/proxyApi': {
+        target: 'http://dev.xxx.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '/proxyApi': ''
+        }
+      }
+    }
+  }
+
 })
